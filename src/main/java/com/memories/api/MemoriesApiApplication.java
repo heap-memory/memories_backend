16ce7bfea5ae105d291e995d201e43cee7179b38
developiaa <developiaa@gmail.com>
2022-04-26 22:05:41 +0900

    first commit

---
+++ src/main/java/com/memories/api/MemoriesApiApplication.java
@@ -0,0 +1,13 @@
+package com.memories.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoriesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/memories/api/MemoriesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memories.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
